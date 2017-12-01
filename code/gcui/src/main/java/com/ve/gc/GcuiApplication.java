015732c79bdf0add689942324b11e0a61c78e05b
wrpys <496732709@qq.com>
2017-12-01 16:01:54 +0800

    项目

---
+++ code/gcui/src/main/java/com/ve/gc/GcuiApplication.java
@@ -0,0 +1,17 @@
+package com.ve.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wrp
+ * @Date 2017/12/01
+ */
+@SpringBootApplication
+public class GcuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcuiApplication.class, args);
+    }
+
+}
---
+++ code/gcui/src/test/java/com/ve/gc/Test.java
@@ -0,0 +1,9 @@
+package com.ve.gc;
+
+/**
+ * @author wrp
+ * @Description com.wrpys.app.wx.Test
+ * @Date 2017/11/7
+ */
+public class Test {
+}
